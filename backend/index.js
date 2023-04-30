268685e29e128e633c14469cebb32d791590f6f9
Abrar Zaman <abrarzaman2003@gmail.com>
2023-04-30 12:31:51 -0500

    added a bunch of backend functions for the website to use

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,79 @@ connection.connect((err) => {
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  connection.query('SELECT * FROM GAME', (err, results) => {
+// gets all the games from the database and sorts them by release date
+
+app.get('/games', (req, res) => {
+  connection.query(
+    'SELECT * FROM GAME ORDER BY GAME.RELEASE_DATE DESC;',
+     (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
+
+// gets a game by its name
+app.get('/games/:name', (req, res) => {
+    connection.query(
+      'SELECT * FROM GAME WHERE GAME.ID ='+[req.params.name],
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+});
+
+// a user can add a game to their wishlist
+app.post('/wishlist/:gid/:uid'), (req, res) => {
+    connection.query(
+      'INSERT INTO WISHLISTS (GID, UID) VALUES (' + [req.params.gid] + ',' + [req.params.uid] + ')', 
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+};
+
+// a user can view their wishlist
+app.get('/wishlist/:uid', (req, res) => {
+    connection.query(
+      'SELECT * FROM WISHLISTS WHERE UID ='+ [req.params.uid],
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+});
+
+// a user can remove a game from their wishlist
+app.delete('/wishlist/:gid/:uid'), (req, res) => {
+    connection.query(
+      'DELETE FROM WISHLISTS WHERE GID ='+ [req.params.gid] +'' + 'AND UID ='+ [req.params.uid], 
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+};
+
+// a user can view games of a certain genre
+app.get('/genre/:genre', (req, res) => {
+    connection.query(
+      'SELECT * FROM GAME WHERE GAME.TYPE ='+ [req.params.genre],
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+});
+
+// a user can view games of a certain publisher
+app.get('/publisher/:publisherID', (req, res) => {
+    connection.query(
+      'SELECT * FROM GAME JOIN PUBLISHERS ON GAME.PID ='+ [req.params.publisherID],
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+});
+
+
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
